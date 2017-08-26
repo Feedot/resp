8722de652de90061781a68a202d684ea6ab759fa
1 <a1@MacBook-Air-1.local>
2017-08-26 23:01:17 +0300

    first

---
+++ src/js/main.js
@@ -0,0 +1,29 @@
+class Change{
+    constructor(){
+    }
+    createColumn(count,row,i){
+        let col = document.createElement('div');
+        col.className = 'col col-xx-'+count;
+        col.innerHTML = i+1;
+        row.appendChild(col);
+    }
+    innerRow(){
+        let count = document.querySelector('select').value,
+            self = this;
+        console.log(count);
+        Array.from(document.getElementsByClassName('row')).map((row)=>{
+            row.innerHTML = '';
+            for (let i = 0; i<count;i++){
+                self.createColumn(count,row,i)
+            }
+        })
+    }
+}
+window.onload = function () {
+    (function(){
+        let changeQuantity = new Change()
+        document.querySelector('select').onchange = ()=>{
+            changeQuantity.innerRow()
+        }
+    }())
+}
